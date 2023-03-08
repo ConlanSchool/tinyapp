3ffaa809ada30d0766d098490d89805852278e63
ConlanSchool <conlan.school@gmail.com>
2023-03-08 13:32:21 -0800

    Overwrite branch error

---
+++ helpers.js
@@ -0,0 +1,13 @@
+//const { users } = require("./express_server");
+
+function generateRandomString() {
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
+
+module.exports = { generateRandomString };
