dbea890a3d52f9b5045cea161fef673e37329cab
ConlanSchool <conlan.school@gmail.com>
2023-03-08 13:30:42 -0800

    Overwrite branch error

--- express_server.js
+++ express_server.js
@@ -1,28 +1,37 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const { generateRandomString } = require("./helpers");
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+const users = {};
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-function generateRandomString() {
-  const chars =
-    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let result = "";
-  for (let i = 0; i < 6; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
+//
+//
+//
 
-app.use(express.urlencoded({ extended: true }));
+app.post("/login", (req, res) => {
+  const { username } = req.body; // Get the value of the "username" field from the request body
+  res.cookie("username", username); // Set a cookie named "username" with the submitted value
+  res.redirect("/urls");
+});
+
+//
+//
+//
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.user) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
 app.listen(PORT, () => {
@@ -33,12 +42,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
@@ -59,6 +65,10 @@ app.post("/urls", (req, res) => {
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
+//
+//
+//
+
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
@@ -68,3 +78,15 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
+
+app.post("/urls/:id", (req, res) => {
+  const newURL = req.body.longURL;
+  const newShort = generateRandomString();
+
+  urlDatabase[newShort] = newURL;
+  delete urlDatabase[req.params.id];
+
+  res.redirect("/urls");
+});
+
+//module.exports = { users };
